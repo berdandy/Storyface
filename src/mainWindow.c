1755c99cfce28c2b416f2c6f7332c867d3470a0a
Andy Berdan <andy@berdan.ca>
2015-09-30 22:40:56 -0400

    Added story text display

--- src/mainWindow.c
+++ src/mainWindow.c
@@ -3,25 +3,41 @@
 
 static Window *s_mainWindow;
 static TextLayer *s_timeLayer;
+static TextLayer *s_storyTitleLayer;
+static TextLayer *s_storyTextLayer;
 
 void mainWindowLoad(Window *window) {
-  // Create time TextLayer
-  s_timeLayer = text_layer_create(GRect(0, 55, 144, 50));
-  text_layer_set_background_color(s_timeLayer, GColorClear);
-  text_layer_set_text_color(s_timeLayer, GColorBlack);
-  text_layer_set_text(s_timeLayer, "00:00");
+	// Create time TextLayer
+	s_timeLayer = text_layer_create(GRect(0, 125, 144, 50));
+	text_layer_set_background_color(s_timeLayer, GColorClear);
+	text_layer_set_text_color(s_timeLayer, GColorBlack);
+	text_layer_set_text(s_timeLayer, "00:00");
+	text_layer_set_font(s_timeLayer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+	text_layer_set_text_alignment(s_timeLayer, GTextAlignmentRight);
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_timeLayer));
+	
+	s_storyTitleLayer = text_layer_create(GRect(0, 0, 144, 24));
+	text_layer_set_background_color(s_storyTitleLayer, GColorClear);
+	text_layer_set_text_color(s_storyTitleLayer, GColorBlack);
+	text_layer_set_text(s_storyTitleLayer, "Story Title");	
+	text_layer_set_font(s_storyTitleLayer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+	text_layer_set_text_alignment(s_storyTitleLayer, GTextAlignmentCenter);
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_storyTitleLayer));
 
-  // Improve the layout to be more like a watchface
-  text_layer_set_font(s_timeLayer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(s_timeLayer, GTextAlignmentCenter);
-
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_timeLayer));
+	// Create story TextLayer
+	s_storyTextLayer = text_layer_create(GRect(12, 24, 120, 120));
+	text_layer_set_background_color(s_storyTextLayer, GColorClear);
+	text_layer_set_text_color(s_storyTextLayer, GColorBlack);
+	text_layer_set_text(s_storyTextLayer, "Story Text 1\nThis is the first note\ntest test test\n4\n5\n6");	
+	text_layer_set_font(s_storyTextLayer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+	text_layer_set_text_alignment(s_storyTextLayer, GTextAlignmentLeft);
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_storyTextLayer));
 }
 
 void mainWindowUnload(Window *window) {
-    // Destroy TextLayer
     text_layer_destroy(s_timeLayer);
+	text_layer_destroy(s_storyTitleLayer);	
+	text_layer_destroy(s_storyTextLayer);
 }
 
 void initMainWindow() {
